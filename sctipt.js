36852ddf6bdaf0bcbca71595388e5cc029af13ac
HamzaMemon007 <162428814+HamzaMemon007@users.noreply.github.com>
2025-01-21 00:21:02 +0500

    Mcqs Tester Made

---
+++ sctipt.js
@@ -0,0 +1,18 @@
+/* 
+let Correct = "correct"
+
+function Check(e) {
+    e.preventDefault();
+    let answer = document.getElementsByName("FirstQuestion");
+
+    for (let i = 0; i < answer.length; i++) {
+       if (answer[i].checked) {
+        if (answer[i].value == Correct) {
+            alert("Correct")
+        } else {
+            alert("Wrong")
+        }
+       }
+    }
+}
+    */
